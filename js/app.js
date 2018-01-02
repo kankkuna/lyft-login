95c52a0755009703429711de06ce78ee1f70bf37
kankkuna <karina842@hotmail.com>
2018-01-02 10:15:07 -0500

    gh-pages

---
+++ js/app.js
@@ -0,0 +1,9 @@
+/*Efecto Splash*/
+$(document).ready(function(){
+  setTimeout(function(){
+    $('#splash').fadeOut(700);}, 2000);
+
+      setTimeout(function() {
+        $('#splash2').fadeOut(900);
+      }, 5000);
+});
